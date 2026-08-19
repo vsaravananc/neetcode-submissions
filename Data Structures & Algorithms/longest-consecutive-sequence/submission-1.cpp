965d0d4da5f8b44f10d708c02cefaa7ac610da0b
Saravanan v <v.saravanan.c@gmail.com>
2026-08-19 08:40:58 +0530

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+      vector<int> start;
+    set<int> mind;
+    int output , realOut , i;
+    output = 1;
+    realOut = i = 0;
+
+    for(int i = 0; i < nums.size() ; i++){
+        mind.insert(nums[i]);
+    }
+
+    for(auto i : mind){
+        int z = i - 1;
+        if(mind.count(z) == 0){
+            start.push_back(i);
+        }
+    }
+
+    while( i < start.size()){
+        int u = start[i] + output;
+        if(mind.count(u) == 1){
+            output++;
+        }else{
+            if(realOut < output){
+                realOut = output;
+            }
+            output = 0;
+            i++;
+        }
+    }
+    return realOut;
+    }
+};
