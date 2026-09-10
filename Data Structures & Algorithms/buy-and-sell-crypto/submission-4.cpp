23e957066b78ced4f06388d6eee90182a7964190
Saravanan v <v.saravanan.c@gmail.com>
2026-09-10 10:09:19 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+     int maxProfit(vector<int>& prices) {
+        if(prices.size() <= 1) return 0;
+        int profit , left , right;
+        profit = left = 0;
+        while(right < prices.size()){
+            int buy = prices[left];
+            int sell = prices[right];
+             if(buy > sell){
+                left = right;
+            }else{
+                profit = max(profit, sell - buy);
+            }
+            right++;
+        }
+        return profit;
+    }
+};
